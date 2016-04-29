88db7744d8e8da8e4580274ccdf3d42ae1519561
Marlan Ball <wyndows2k@earthlink.net>
2016-04-29 10:05:30 -0600

    updated the timeline and conceptual model and usecases

--- epic/timeline.php
+++ epic/timeline.php
@@ -4,20 +4,16 @@
 <h3>Week 1- April 25th - 29th</h3>
 <p>Plan website needs. Decide plan of action.</p>
 <h3>Week 2- May 2nd - 6th</h3>
-<p>Rinse & Repeat</p>
-<p>Depending on Bridge input.</p>
-<p>Start mySQL</p>
+<p>mySQL</p>
+<p>PHP classes.</p>
 <h3>Week 3- May 9th - 13th</h3>
-<p>Development of mySQL</p>
-<p>Maybe page planning</p>
-<h3>Week 4- May 16th - 20th</h3>
-<p>Page planning</p>
-<p>Maybe content</p>
+<p>PHP classes</p>
+<h3>Week 4- May 16th - 20th </h3>
+<p>API</p>
 <h3>Week 5- May 23rd - 27th</h3>
-<p>Content</p>
-<p>Maybe Soft launch</p>
+<p>API</p>
 <h3>Week 6- May 30th - June 3rd</h3>
-<p>Revision (Rinse and Repeat)</p>
-<p>Final Launch</p>
+<p>API</p>
+<p>Front End</p>
 <h3>Week 7- June 6th - 10th</h3>
-<p>Presentation</p>
+<p>Front End</p>
