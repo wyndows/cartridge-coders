96992cabe6c4ff9fc9ef7f3f9646ee22bf437f29
Marlan Ball <wyndows2k@earthlink.net>
2016-04-29 06:22:14 -0600

    Updated use cases and some general edits

--- epic/useCaseAdmin.php
+++ epic/useCaseAdmin.php
@@ -1,4 +1,4 @@
 <p class="title">Use Case Admin</p>
 <br>
-<p>A user has been getting many complaints about his excessive returns of working items.  James Dean looks at his email and sees that one of the Cartridge Coders users has exceeded the site rules regarding returns.</p>
-<p>He logs into the site and goes into the admin area and pulls up the users account and info.  He sets the user profile in the database so that they can no longer buy or sell on the site.</p>
+<p>A complaint has been logged in the messaging system about an inappropriate listing of an item.  James Dean looks at his email and sees that the user who posted has refused to unlist the item.</p>
+<p>He logs into the site and goes into the admin area and pulls up the users account and info.  As per the site use agreement, admin have the right to remove users if they refuse to unlist inappropriate items.  He sets the user profile in the database so that they can no longer buy or sell on the site.</p>
--- epic/useCaseBuyer.php
+++ epic/useCaseBuyer.php
@@ -1,6 +1,6 @@
 
 <p class="title">Use Case Buyer</p>
 <br>
-<p>George McDonald is looking to increase his game choices for his classic game console. He regularly uses the Cartridge Coders website as a place to look for cartridges for old consoles.</p>
+<p>George McDonald is looking to increase his game choices for his classic game console. He regularly uses the Cartridge Coders website as a place to look for cartridges.  He is using his laptop to sign into Cartridge Coders.</p>
 <p>He goes to the site and looks around at the various listings.</p>
 <p>After looking at several, he decides on one and fills out the form for buying and uses his PayPal account to complete the transaction.</p>
