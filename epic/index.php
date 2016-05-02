6e5109fad1328329497fb14bb5ab69ecac4fc458
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-02 16:47:56 -0600

    added test https://app.asana.com/0/117435667927150/119382799305961

--- epic/index.php
+++ epic/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="../css/style.css">
+		<link rel="stylesheet" href="../public_html/css/style.css">
 		<title>Cartridge Coders</title>
 	</head>
 
