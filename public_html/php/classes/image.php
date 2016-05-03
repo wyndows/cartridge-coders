b60a6bd02b8f803c94035ce2f3362492b6bb146c
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-03 14:23:46 -0600

    image class draft :: https://app.asana.com/0/117435667927150/119382799305973

--- public_html/php/classes/image.php
+++ public_html/php/classes/image.php
@@ -1,1 +1,31 @@
 <?php
+namespace Edu\Cnm\Ddeleeuw\Cartridge;
+
+/**
+ * Class for Images
+ * @author Donald DeLeeuw <donald.deleeuw@gmail.com>
+ */
+
+class Image{
+
+	/**
+	 * id for image, this is the primary key
+	 * @var int $imageId
+	 */
+	private $imageId;
+
+	/**
+	 * file name of image
+	 * @var varchar(128) $imageFileName
+	 */
+	private $imageFileName;
+
+	/**
+	 * file type of image
+	 * @var varchar(10) $imageType
+	 */
+	private $imageType;
+}
+
+
+
