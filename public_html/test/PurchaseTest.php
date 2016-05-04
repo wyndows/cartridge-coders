e472054d859f283daa694da9b114c26945abce38
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-04 06:16:24 -0600

    createing the test for Purchase class https://app.asana.com/0/117435667927150/119382799305975

---
+++ public_html/test/PurchaseTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Edu\Cnm\CartridgeCoders\Test
+
+//parts of this code have been modified from the original, created by Dylan Mcdonald and taken from https://bootcamp-coders.cnm.edu
+
+// grab the project test parameters
+require_once("CartridgeCodersTest.php");
+
+//grab the cal under scrutiny
+require_once(dirname(__DIR__) . "/php/classes/autoload.php");
+
+/**
+ * full PHPUnit test for the Purchase class
+ *
+ * This is a an attempt at TDD testing.
+ *
+ *@see Purchase
+ *@author Elliot Murrey <emurrey@cnm.edu
+ **/
+class Purchase extends CartridgeCodersTest {
+	/**
+	 * content of the Purchase
+	 * @var string $VALID_PURCHASE
+	 **/
+	protected $VALID_PURCHASE
+}
