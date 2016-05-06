2bec2fadc5631c38e811eba535657dacecc4fa83
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-05 19:47:14 -0600

    ProductPurchase class draft update https://app.asana.com/0/117435667927150/119382799305977

---
+++ public_html/php/classes/ProductPurchase.php
@@ -0,0 +1,15 @@
+<?php
+namespace Edu\Cnm\CartridgeCoders;
+
+/**
+ * Class for ProductPurchase
+ * @author Donald DeLeeuw <donald.deleeuw@gmail.com> based on code by Dylan McDonald <dmcdonald21@cnm.edu>
+ */
+
+class ProductPurchase implements \JsonSerializable {
+
+
+
+
+
+
