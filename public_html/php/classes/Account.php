7187c94031680f5d312cc36fdd797ef455a1e85f
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-06 11:00:08 -0600

    Account class draft update https://app.asana.com/0/117435667927150/119382799305961

--- public_html/php/classes/Account.php
+++ public_html/php/classes/Account.php
@@ -6,7 +6,7 @@ namespace Edu\Cnm\CartridgeCoders;
  * @author Donald DeLeeuw <donald.deleeuw> based on code by Dylan McDonald <dmcdonad21@cnm.edu>
  */
 
-class Account implements \JsonSerializable{
+class Account implements \JsonSerializable {
 
 	/**
 	 * id for account, this is the primary key
@@ -50,9 +50,42 @@ class Account implements \JsonSerializable{
 	private $accountUserName;
 
 	/**
-	 * 
+	 * constructor for Account class
+	 * @param int|null $newAccountId - id of account or null if new account - primary key
+	 * @param int|null $newAccountImageId - id of image - this is a foreign key
+	 * @param int|null $newAccountActive - flag for account active
+	 * @param int|null $newAccountAdmin - flag for account admin
+	 * @param string $newAccountName - user's name
+	 * @param string $newAccountPpEmail - user's paypal email address
+	 * @param string $newAccountUserName - user's chosen user name
+	 * @throws \InvalidArgumentException - if data types are not valid
+	 * @throws \RangeException - if values are out of range (strings too long, negative numbers, etc.)
+	 * @throws \TypeError - if data types violate type hints
+	 * @throws \Exception - catch all if another error occurs
 	 */
-
+	public function __construct(int $newAccountId = null, int $newAccountImageId = null, int $newAccountActive = null, int $newAccountAdmin = null, string $newAccountName, string $newAccountPpEmail, string $newAccountUserName) {
+		try {
+			$this->setAccountId($newAccountId);
+			$this->setAccountImageId($newAccountImageId);
+			$this->setAccountActive($newAccountActive);
+			$this->setAccountAdmin($newAccountAdmin);
+			$this->setAccountName($newAccountName);
+			$this->setAccountPpEmail($newAccountPpEmail);
+			$this->setAccountUserName($newAccountUserName);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			// rethrow the exception to the caller
+			throw (new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			// rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			// rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
 
 
 }
