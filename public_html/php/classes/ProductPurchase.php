16dcf76508134fc01e4a36a68414d4eefe8b21f9
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-05 20:47:26 -0600

    ProductPurchase class draft update https://app.asana.com/0/117435667927150/119382799305977

--- public_html/php/classes/ProductPurchase.php
+++ public_html/php/classes/ProductPurchase.php
@@ -8,6 +8,43 @@ namespace Edu\Cnm\CartridgeCoders;
 
 class ProductPurchase implements \JsonSerializable {
 
+// /**
+//  * id for productPurchasePurchaseId.productPurchaseProductId, this is the primary key
+//  * @var int $productPurchasePurchaseIdProductPurchaseProductId
+//  */
+// private $productPurchasePurchaseIdProductPurchaseProductId;
+
+
+	/**
+	 * id for productId, this is a foreign key
+	 * @var int $productPurchaseProductId
+	 */
+	private $productPurchaseProductId;
+
+	/**
+	 * id for purchaseId, this is a foreign key
+	 * @var int $productPurchasePurchaseId
+	 */
+	private $productPurchasePurchaseId;
+
+	/**
+	 * constructor for ProductPurchase class
+//		* @param int|null $productPurchasePurchaseIdProductPurchaseProductId - composit of purchaseId and priductId - this is the primary key
+	 * @param int|null $productPurchaseProductId - primary key of product table - this is a foreign key
+	 * @param int|null $productPurchasePurchaseId - primary key of purchase table - this is a foreign key
+	 * @throws \InvalidArgumentException - if data types are not valid
+	 * @throws \RangeException - if values are out of range (strings too long, negative numbers, etc.)
+	 * @throws \TypeError - if data types violate type hints
+	 * @throws \Exception - catch all if another error occurs
+	 **/
+
+
+
+
+
+
+}
+
 
 
 
