0cb8db4896b30a9d49ab0d53901a2f238e663263
Marlan Ball <wyndows2k@earthlink.net>
2016-05-09 14:37:32 -0600

    started product class, setup variables and constructor https://app.asana.com/0/117435671931096/119382799305967

---
+++ public_html/php/classes/Product.php
@@ -0,0 +1,111 @@
+<?php
+namespace Edu\Cnm\CartridgeCoders;
+
+require_once ("autoload.php");
+
+/**
+ * Product class for Cartridge Coders capstone project
+ *
+ * @author Marlan Ball <wyndows@earthlink.net>
+ * @version 1.0.0
+ */
+class Product implements \JsonSerializable {
+	// use ValidateDate;
+
+	/**
+	 * id for this product; this is the primary key
+	 * @var int $productId
+	 */
+	private $productId;
+	/**
+ * the account id of the product (foreign key)
+ * @var string $productAccountId
+ */
+	private $productAccountId;
+	/**
+ * the image id of the product (foreign key)
+ * @var string $productImageId
+ */
+	private $productImageId;
+	/**
+	 * the admin fee of the product
+	 * @var string $productAdminFee
+	 */
+	private $productAdminFee;
+	/**
+ * the description of the product
+ * @var string $productDescription
+ */
+	private $productDescription;
+	/**
+	 * the price of the product
+	 * @var string $productPrice
+	 */
+	private $productPrice;
+	/**
+	 * the shipping of the product
+	 * @var string $productShipping
+	 */
+	private $productShipping;
+	/**
+	 * the sold status of the product
+	 * @var string $productSold
+	 */
+	private $productSold;
+	/**
+	 * the title of the product
+	 * @var string $productTitle
+	 */
+	private $productTitle;
+
+	/**
+	 * constructor for this product
+	 *
+	 * @param int|null $newProductId id of this product or null if a new product
+	 * @param int $newProductAccountId id of account holder selling product
+	 * @param int $newProductImageId id of image attached to product
+	 * @param decimal $newProductAdminFee percent of sale listing fee
+	 * @param string $newProductDescription string containing description of the product
+	 * @param decimal $newProductPrice cost of product
+	 * @param decimal $newProductShipping shipping cost of product
+	 * @param int $newProductSold sold status of product
+	 * @param string $newProductTitle title of product listing
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g. strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 */
+	public function __construct(int $newProductId = null, int $newProductAccountId, int $newProductImageId, decimal $newProductAdminFee, string $newProductDescription, decimal $newProductPrice, decimal $newProductShipping, int $newProductSold, string $newProductTitle) {
+		try {
+			$this->setProductId($newProductId);
+			$this->setProductAccountId($newProductAccountId);
+			$this->setProductImageId($newProductImageId);
+			$this->setProductAdminFee($newProductAdminFee);
+			$this->setProductDescription($newProductDescription);
+			$this->setProductPrice($newProductPrice);
+			$this->setProductShipping($newProductShipping);
+			$this->setProductSold($newProductSold);
+			$this->setProductTitle($newProductTitle);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			//rethrow the exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			//rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			//rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			//rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
+
+
+
+
+
+
+
+?>
