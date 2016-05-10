b435788c7c24b8296a922f3c365dc68fc36143c5
Marlan Ball <wyndows2k@earthlink.net>
2016-05-09 19:34:46 -0600

    added get product by product admin fee method https://app.asana.com/0/117435671931096/119382799305967

--- public_html/php/classes/Product.php
+++ public_html/php/classes/Product.php
@@ -567,7 +567,53 @@ class Product implements \JsonSerializable {
 		return ($products);
 	}
 
-	
+	/**
+	 * get the product by productAdminFee
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $productAdminFee product admin fee to search for
+	 * @return Product|null product found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 */
+	public static function getProductByAdminFee(\PDO $pdo, int $productAdminFee) {
+		// sanitize the productAdminFee before searching
+		if($productAdminFee < 0) {
+			throw(new \PDOException("product admin fee is not positive"));
+		}
+		if($productAdminFee >= 1000) {
+			throw(new \PDOException("product admin fee is too high"));
+		}
+
+		// create query template
+		$query = "SELECT productId, productAccountId, productImageId, productAdminFee, productDescription, productPrice, productShipping, productSold, productTitle FROM product WHERE productAdminFee = :productAdminFee";
+		$statement = $pdo->prepare($query);
+
+		// bind the product admin fee to the place holder in the template
+		$parameters = array("productAdminFee" => $productAdminFee);
+		$statement->execute($parameters);
+
+		// build an array of products
+		$products = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$product = new Product($row["productId"], $row["productAccountId"], $row["productImageId"], $row["productAdminFee"], $row["productDescription"], $row["productPrice"], $row["productShipping"], $row["productSold"], $row["productTitle"]);
+				$products[$products->key()] = $product;
+				$products->next();
+			}
+			catch
+			(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($products);
+	}
+
+
+
+
 	/**
 	 * formats the state variables for JSON serialization
 	 *
