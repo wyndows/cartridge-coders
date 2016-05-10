970421cee7f748a912c43183f9e938c0d95db9c6
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-10 16:06:37 -0600

    account class test draft update https://app.asana.com/0/117435667927150/119382799305961

--- public_html/test/AccountTest.php
+++ public_html/test/AccountTest.php
@@ -152,20 +152,20 @@ class AccountTest extends CartridgeCodersTest {
 	/**
 	 * test creating a AccountId and then deleting it
 	 **/
-	public function testDeleteVAccountId() {
+	public function testDeleteAccountId() {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("account");
 
 		// create a new AccountId and insert into mySQL
-		$category = new Account(null, $this->AccountId);
-		$category->insert($this->getPDO());
+		$account = new Account(null, $this->AccountId);
+		$account->insert($this->getPDO());
 
 		// delete the AccountId from mySQL
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("account"));
-		$category->delete($this->getPDO());
+		$account->delete($this->getPDO());
 
 		// grab the data from mySQL and enforce that the AccountId does not exist
-		$pdoCategory = Category::getAccountIdByAccountId($this->getPDO(), $account->getAccountId());
+		$pdoAccount = Account::getAccountIdByAccountId($this->getPDO(), $account->getAccountId());
 		$this->assertNull($pdoAccountId);
 		$this->assertEquals($numRows, $this->getConnection()->getRowCount("account"));
 	}
@@ -265,20 +265,20 @@ class AccountTest extends CartridgeCodersTest {
 	/**
 	 * test creating a AccountImageId and then deleting it
 	 **/
-	public function testDeleteVAccountImageId() {
+	public function testDeleteAccountImageId() {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("account");
 
 		// create a new AccountImageId and insert into mySQL
-		$category = new Account(null, $this->AccountImageId);
-		$category->insert($this->getPDO());
+		$account = new Account(null, $this->AccountImageId);
+		$account->insert($this->getPDO());
 
 		// delete the AccountImageId from mySQL
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("account"));
-		$category->delete($this->getPDO());
+		$account->delete($this->getPDO());
 
 		// grab the data from mySQL and enforce that the AccountImageId does not exist
-		$pdoCategory = Category::getAccountImageIdByAccountImageId($this->getPDO(), $account->getAccountImageId());
+		$pdoAccount = Account::getAccountImageIdByAccountImageId($this->getPDO(), $account->getAccountImageId());
 		$this->assertNull($pdoAccountImageId);
 		$this->assertEquals($numRows, $this->getConnection()->getRowCount("account"));
 	}
@@ -350,20 +350,20 @@ class AccountTest extends CartridgeCodersTest {
 	/**
 	 * test creating a AccountActive and then deleting it
 	 **/
-	public function testDeleteVAccountActive() {
+	public function testDeleteAccountActive() {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("account");
 
 		// create a new AccountActive and insert into mySQL
-		$category = new Account(null, $this->AccountActive);
-		$category->insert($this->getPDO());
+		$account = new Account(null, $this->AccountActive);
+		$account->insert($this->getPDO());
 
 		// delete the AccountActive from mySQL
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("account"));
-		$category->delete($this->getPDO());
+		$account->delete($this->getPDO());
 
 		// grab the data from mySQL and enforce that the AccountActive does not exist
-		$pdoCategory = Category::getAccountActiveByAccountActive($this->getPDO(), $account->getAccountActive());
+		$pdoAccount = Account::getAccountActiveByAccountActive($this->getPDO(), $account->getAccountActive());
 		$this->assertNull($pdoAccountActive);
 		$this->assertEquals($numRows, $this->getConnection()->getRowCount("account"));
 	}
@@ -407,30 +407,261 @@ class AccountTest extends CartridgeCodersTest {
 	}
 
 
+// ------------------------------------------- AccountName -------------------------------------------
+
+	/**
+	 * updates AccountName in mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL errors occure
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function updateAccountName(\PDO $pdo) {
+
+		// enforce the AccountName is not null (don't update whats not there)
+		if($this->accountName === null) {
+			throw(new \PDOException("unable to update account name that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE account SET accountName = :accountName WHERE accountName = :accountName";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders
+		$parameters = ["accountId" => $this->accountId, "accountImageId" => $this->accountImageId, "accountAdmin" => $this->accountAdmin, "accountName" => $this->accountName, "accountPpEmail" => $this->accountPpEmail, "accountUserName" => $this->accountUserName];
+		$statement->execute($parameters);
+	}
 
 
+	/**
+	 * test creating a AccountName and then deleting it
+	 **/
+	public function testDeleteAccountName() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("account");
+
+		// create a new AccountName and insert into mySQL
+		$account = new Account(null, $this->AccountName);
+		$account->insert($this->getPDO());
+
+		// delete the AccountName from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("account"));
+		$account->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce that the AccountName does not exist
+		$pdoAccount = Account::getAccountNameByAccountName($this->getPDO(), $account->getAccountName());
+		$this->assertNull($pdoAccountName);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("account"));
+	}
+
+	/**
+	 * gets the AccountName by account id
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $accountName - account name search for
+	 * @param int $accountId - prim key
+	 * @return int|null - id found or null if not
+	 * @throws \PDOException when mySQL related error occurs
+	 * @throws \TypeError when variables are not the correct data type
+	 */
+
+	public static function getAccountNameByAccountId(\PDO $pdo, int $accountId) {
+		// sanitize the accountId before searching
+		if($accountId <= 0) {
+			throw(new \PDOException("account id is not positive"));
+		}
+		// create query template
+		$query = "SELECT accountId, accountImageId, accountActive, accountAdmin, accountName, accountPpEmail, accountUserName FROM account WHERE accountId = :accountId";
+		$statement = $pdo->prepare($query);
+
+		// bind the account id to the place holder in the template
+		$parameters = array("accountId" => $accountId);
+		$statement->execute($parameters);
+
+		// grab the id from mySQL
+		try {
+			$accountId = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$accountId = new Account($row["accountId"], $row["accountImageId"], $row["accountActive"], $row["accountAdmin"], $row["accountName"], $row["accountPpEmail"], $row["accountUserName"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($accountId);
+	}
 
 
+// ------------------------------------------- AccountPpEmail -------------------------------------------
 
-//* 		 DONE		int|null AccountId - id of account or null if new account - primary key
-//* 		 DONE		int|null AccountImageId - id of image - this is a foreign key
-//* 		 DONE		int|null AccountActive - flag for account active
-//* int|null AccountAdmin - flag for account admin
-//* string AccountName - user's name
-//* string AccountPpEmail - user's paypal email address
-//* string AccountUserName - user's chosen user name
+	/**
+	 * updates AccountPpEmail in mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL errors occure
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function updateAccountPpEmail(\PDO $pdo) {
 
-// *
-// * insert (x1)
-// * update
-// * delete
-// * get by attribute
-// * get all (x1)
+		// enforce the AccountPpEmail is not null (don't update whats not there)
+		if($this->accountPpEmail === null) {
+			throw(new \PDOException("unable to update account pay pal email that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE account SET accountPpEmail = :accountPpEmail WHERE accountPpEmail = :accountPpEmail";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders
+		$parameters = ["accountId" => $this->accountId, "accountImageId" => $this->accountImageId, "accountAdmin" => $this->accountAdmin, "accountName" => $this->accountName, "accountPpEmail" => $this->accountPpEmail, "accountUserName" => $this->accountUserName];
+		$statement->execute($parameters);
+	}
+
+
+	/**
+	 * test creating a AccountPpEmail and then deleting it
+	 **/
+	public function testDeleteAccountPpEmail() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("account");
+
+		// create a new AccountPpEmail and insert into mySQL
+		$account = new Account(null, $this->AccountPpEmail);
+		$account->insert($this->getPDO());
+
+		// delete the AccountPpEmail from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("account"));
+		$account->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce that the AccountPpEmail does not exist
+		$pdoAccount = Account::getAccountPpEmailByAccountPpEmail($this->getPDO(), $account->getAccountPpEmail());
+		$this->assertNull($pdoAccountPpEmail);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("account"));
+	}
+
+	/**
+	 * gets the AccountPpEmail by account id
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $accountPpEmail - account pay pal email search for
+	 * @param int $accountId - prim key
+	 * @return int|null - id found or null if not
+	 * @throws \PDOException when mySQL related error occurs
+	 * @throws \TypeError when variables are not the correct data type
+	 */
+
+	public static function getAccountPpEmailByAccountId(\PDO $pdo, int $accountId) {
+		// sanitize the accountId before searching
+		if($accountId <= 0) {
+			throw(new \PDOException("account id is not positive"));
+		}
+		// create query template
+		$query = "SELECT accountId, accountImageId, accountActive, accountAdmin, accountName, accountPpEmail, accountUserName FROM account WHERE accountId = :accountId";
+		$statement = $pdo->prepare($query);
+
+		// bind the account id to the place holder in the template
+		$parameters = array("accountId" => $accountId);
+		$statement->execute($parameters);
+
+		// grab the id from mySQL
+		try {
+			$accountId = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$accountId = new Account($row["accountId"], $row["accountImageId"], $row["accountActive"], $row["accountAdmin"], $row["accountName"], $row["accountPpEmail"], $row["accountUserName"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($accountId);
+	}
+
+
+// ------------------------------------------- AccountUserName -------------------------------------------
+
+	/**
+	 * updates AccountUserName in mySQL
+	 * @param \PDO $pdo PDO connection object
+	 * @throws \PDOException when mySQL errors occure
+	 * @throws \TypeError if $pdo is not a PDO connection object
+	 */
+	public function updateAccountUserName(\PDO $pdo) {
+
+		// enforce the AccountUserName is not null (don't update whats not there)
+		if($this->accountUserName === null) {
+			throw(new \PDOException("unable to update account user name that does not exist"));
+		}
+
+		// create query template
+		$query = "UPDATE account SET accountUserName = :accountUserName WHERE accountUserName = :accountUserName";
+		$statement = $pdo->prepare($query);
+
+		// bind the member variables to the place holders
+		$parameters = ["accountId" => $this->accountId, "accountImageId" => $this->accountImageId, "accountAdmin" => $this->accountAdmin, "accountName" => $this->accountName, "accountPpEmail" => $this->accountPpEmail, "accountUserName" => $this->accountUserName];
+		$statement->execute($parameters);
+	}
+
+
+	/**
+	 * test creating a AccountUserName and then deleting it
+	 **/
+	public function testDeleteAccountUserName() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("account");
+
+		// create a new AccountUserName and insert into mySQL
+		$account = new Account(null, $this->AccountUserName);
+		$account->insert($this->getPDO());
+
+		// delete the AccountUserName from mySQL
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("account"));
+		$account->delete($this->getPDO());
+
+		// grab the data from mySQL and enforce that the AccountUserName does not exist
+		$pdoAccount = Account::getAccountUserNameByAccountUserName($this->getPDO(), $account->getAccountUserName());
+		$this->assertNull($pdoAccountUserName);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("account"));
+	}
+
+	/**
+	 * gets the AccountUserName by account id
+	 * @param \PDO $pdo PDO connection object
+	 * @param int $accountUserName - account user name search for
+	 * @param int $accountId - prim key
+	 * @return int|null - id found or null if not
+	 * @throws \PDOException when mySQL related error occurs
+	 * @throws \TypeError when variables are not the correct data type
+	 */
+
+	public static function getAccountUserNameByAccountId(\PDO $pdo, int $accountId) {
+		// sanitize the accountId before searching
+		if($accountId <= 0) {
+			throw(new \PDOException("account id is not positive"));
+		}
+		// create query template
+		$query = "SELECT accountId, accountImageId, accountActive, accountAdmin, accountName, accountPpEmail, accountUserName FROM account WHERE accountId = :accountId";
+		$statement = $pdo->prepare($query);
+
+		// bind the account id to the place holder in the template
+		$parameters = array("accountId" => $accountId);
+		$statement->execute($parameters);
+
+		// grab the id from mySQL
+		try {
+			$accountId = null;
+			$statement->setFetchMode(\PDO::FETCH_ASSOC);
+			$row = $statement->fetch();
+			if($row !== false) {
+				$accountId = new Account($row["accountId"], $row["accountImageId"], $row["accountActive"], $row["accountAdmin"], $row["accountName"], $row["accountPpEmail"], $row["accountUserName"]);
+			}
+		} catch(\Exception $exception) {
+			// if the row couldn't be converted, rethrow it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+		return ($accountId);
+	}
 
 }
-
-
-
 
 
 ?>
