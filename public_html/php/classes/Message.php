228e90ec1031201c38e389b6ecc49395488584fd
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-11 20:01:18 -0600

    update from scrum https://app.asana.com/0/117435667927150/119382799305969

--- public_html/php/classes/Message.php
+++ public_html/php/classes/Message.php
@@ -1,12 +1,12 @@
 <?php
+
 /**
  * Message class for cartridge coders so accounts may message eachother back and forth
  * @author Elliot Murrey  <emurrey@cnm.edu> based on code from Dylan McDonald <dmcdonald21@cnm.edu>
  **/
-
 class Message implements \JsonSerializable {
 	/**
-	 * the id for the message, this will be the primary key 
+	 * the id for the message, this will be the primary key
 	 * @var int $messageId
 	 **/
 	private $messageId;
@@ -31,10 +31,10 @@ class Message implements \JsonSerializable {
 	 **/
 	private $messageContent;
 	/**
-	 * the id the api mailGun will use 
+	 * the id the api mailGun will use
 	 * @var string $messageMailGunId
 	 **/
-	private $messageMailGunId; 
+	private $messageMailGunId;
 	/**
 	 * what the message will be about
 	 * #var string $messageSubject
@@ -54,7 +54,7 @@ class Message implements \JsonSerializable {
 	 * @throws UnexpectedValueException if any of the parameters are invalid
 	 * @throws TypeError if data violates type hints
 	 **/
-	public function __construct($newMessageId, $newAccountId, $newProductId, $newAccountId2, $newMessageContent, $newMessageMailGunId, $newMessageSubject) {
+	public function __construct($newMessageId, $newSenderId, $newProductId, $newSenderId, $newMessageContent, $newMessageMailGunId, $newMessageSubject) {
 		try {
 			$this->setMessageId($newMessageId);
 			$this->setSenderId($newAccountId);
@@ -71,17 +71,19 @@ class Message implements \JsonSerializable {
 			throw(new \UnexpectedValueException("unable to contruct Message", 0, $exception));
 		} catch(\RangeException $range) {
 			//rethrow the exceptin to the caller
-			throw(new \RangeException($range ->getMessage(), 0, $range));
+			throw(new \RangeException($range->getMessage(), 0, $range));
 		}
-}
+	}
+
 	/**
 	 * accessor method for message Id
 	 *
 	 * @return int value of message id
 	 **/
 	public function getMessageId() {
-		return($this->messageId);
+		return ($this->messageId);
 	}
+
 	/**
 	 *mutator method for message id
 	 * @param int $newMessageId new value of message id
@@ -94,20 +96,22 @@ class Message implements \JsonSerializable {
 			throw(new UnexpectedValueException("MessageId is not a valid integer"));
 		}
 		//confirm message is positive
-		if($newMessageId <=0) {
+		if($newMessageId <= 0) {
 			throw(new \RangeException("message id is not valid"));
 		}
 		//convert and store the message id
 		$this->messageId = intval($newMessageId);
 	}
+
 	/**
 	 * accessor method for sender id
 	 *
 	 * @return int value of sender id
 	 **/
 	public function getSenderId() {
-		return($this->senderId);
+		return ($this->senderId);
 	}
+
 	/**
 	 * mutator method for message sender id
 	 * @param int $newSenderId new value of sender id
@@ -120,20 +124,22 @@ class Message implements \JsonSerializable {
 			throw(new UnexpectedValueException("SenderId is not a valid integer"));
 		}
 		//confirm sender id is positive
-		if($newSenderId <=0) {
+		if($newSenderId <= 0) {
 			throw(new RangeException("sender id is not positive"));
 		}
 		//convert and store the account id
 		$this->senderId = intval($newSenderId);
 	}
+
 	/**
 	 * accessor method for product id
 	 *
 	 * @return int value of product id
 	 **/
 	public function getProductId() {
-		return($this->productId);
+		return ($this->productId);
 	}
+
 	/**
 	 * mutator method for product id
 	 * @param int $newProductId new value of product id
@@ -146,20 +152,22 @@ class Message implements \JsonSerializable {
 			throw(new UnexpectedValueException("ProductId is not a valid integer"));
 		}
 		// confirm product id is positive
-		if($newProductId <=0) {
+		if($newProductId <= 0) {
 			throw(new RangeException("product id is not positive"));
 		}
 		//convert and store the product id
 		$this->productId = intval($newProductId);
 	}
+
 	/**
 	 * accessor method for recipient id
 	 *
 	 * @return int value of recipient id
 	 **/
 	public function getRecipientId() {
-		return($this->recipientId);
+		return ($this->recipientId);
 	}
+
 	/**
 	 * mutator method for message recipient id
 	 * @param int $newRecipientId new value of recipient id
@@ -172,20 +180,22 @@ class Message implements \JsonSerializable {
 			throw(new UnexpectedValueException("RecipientId is not a valid integer"));
 		}
 		//confirm sender id is positive
-		if($newRecipientId <=0) {
+		if($newRecipientId <= 0) {
 			throw(new RangeException("sender id is not positive"));
 		}
 		//convert and store the account id
 		$this->recipientId = intval($newRecipientId);
 	}
+
 	/**
 	 * accessor for message content
 	 *
 	 * @return string value of message content
 	 **/
 	public function getMessageContent() {
-		return($this->messageContent);
+		return ($this->messageContent);
 	}
+
 	/**
 	 * mutator method for message content
 	 *
@@ -206,14 +216,16 @@ class Message implements \JsonSerializable {
 		// store the message content
 		$this->messageContent = $newMessageContent;
 	}
+
 	/**
 	 * accessor for mail gun id
 	 *
 	 * #return string value of message mailgun id
 	 **/
 	public function getMessageMailGunId() {
-		return($this->messageMailGunId);
+		return ($this->messageMailGunId);
 	}
+
 	/**
 	 * mutator method for message mailgun id
 	 *
@@ -228,20 +240,22 @@ class Message implements \JsonSerializable {
 			throw(new UnexpectedValueException("message mailgun id is not a valid string"));
 		}
 		// verify the messge mail gun id is less then 32
-		if($newMessageMailGunId >32) {
+		if($newMessageMailGunId > 32) {
 			throw(new RangeException("invalid mailgunId"));
 		}
 		//store the message mailgun id
 		$this->messageMailGunId = $newMessageMailGunId;
 	}
+
 	/**
 	 * accessor for message subject
 	 *
 	 * @return string value of message subject
 	 **/
-	public function  getMessageSubject() {
-		return($this->messageSubject);
+	public function getMessageSubject() {
+		return ($this->messageSubject);
 	}
+
 	/**
 	 * mutator method for message subject
 	 *
@@ -287,117 +301,15 @@ class Message implements \JsonSerializable {
 		//update the null messageId with what mySQL just gave us
 		$this->messageId = intval($pdo->lastInsertId());
 	}
+	//partyId is SenderId or RecipientId
+	// get messageByMessageId, getMessageByPartyId,
 	/**
-	 * delets this Message from mySQL
+	 * formats the state variables for JSON serialization
 	 *
-	 * @param \PDO $pdo PDO connecction object
-	 * @throws \PDOException when mySQL related errors occur
-	 * @throws \TypeError if $pdo is not a PDO connection object
-	 **/
-	public function delete(\PDO $pdo) {
-		// enforce the messageId is not null (i.E., don't delete a tweet that hasn't been inserted
-		if($this->messageId === null) {
-			throw(new \PDOException("unable to delete a twwet that does not exist"));
-		}
-
-		//create query template
-		$query = "DELETE FROM message WHERE messageId = :messageId";
-		$statement = $pdo->prepare($query);
-
-		//bind the member variable to the place holder in the template
-		$parameters = ["messageId" => $this->messageId];
-		$statement->execute($parameters);
+	 * @return array resulting state variables to serialize
+	 */
+	public function jsonSerialize() {
+		$fields = get_object_vars($this);
+		return($fields);
 	}
-	/**
-	 * updates this Message in mySQL
-	 *
-	 * @param \PDO $pdo PDO connection object
-	 * @throws \PDOException when mySQL related errors occur
-	 * @throws \TypeError if $pdo is not a PDO connection object
-	 **/
-	public function update(\PDO $pdo) {
-		// enforce the messageId is not null (i.e., doon't update a message that hasn't been inserted)
-		if($this->messageId === null) {
-			throw(new\PDOException("unable to updatea message that does not exist"));
-		}
-		/**
-		 * gets the Message by content
-		 *
-		 *@param \PDO $pdo PDO connection object
-		 *@param string $messageContent message content to search for
-		 *@return \SplFixedArray SplFixedArray of messages found
-		 *@throws \PDOException when mySQL related error occur
-		 *@throws \TypeError when variable are not the correct data type
-		 **/
-		public static function getMessageByMessageContent(\PDO $pdo, string $messageContent) {
-			// sanitize the description before searching
-			$messageContent = trim($messageContent);
-			$messageContent = filter_var($messageContent, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-			if(empty($messageContent) === true) {
-				throw(new \PDOException("message content is invalid"));
-			}
-
-			// create query template
-			$query = "SELECT messageId, senderId, productId, messageContent, recipientId, messageMailGunId, messageSubject FROM message WHERE messageContent LIKE :messageContent";
-			$statement = $pdo->prepare($query);
-
-			// bind the message content to the place holder in the template
-			$messagecontent = "%$messageContent%";
-			$parameters = array("messagecontent" => $messageContent);
-			$statement->execute($parameters);
-
-			// build an array of messages
-			$message = new \SPLFixedArray($statement->rowCount());
-			$statement->setFetchMode(\PDO::FETCH_ASSOC);
-			while(($row = $statment->fetch()) !== false) {
-				try {
-					$message = new Messaage($row["messageId"], $row["senderId"], $row["productId"], $row["messageContent"], $row["recipientId"], $row["messageMailGunId"], $row["messageSubject"]);
-					$messages[$message->key()] = $message;
-					$message->next();
-				} catch(\Exception $exception) {
-					// if the row couln't be converted, rethrow it
-					throw(new ]PDOExceptions($exception->getMesage(), 0, $exceptions));
-				}
-			}
-			return($messages);
-		}
-		/**
-		 * gets the Message by MessageSubject
-		 * @param \PDO $pdo PDO connection object
-		 * @param string $messageSubject message subject to search for
-		 * @return Message|null Message found or null if not found
-		 * @throws \PDOException when mySQl related errors occur
-		 * @throws \TypeError when variables are not the correct data type
-		 **/
-		public static functions getMessageByMessageSubject(\PDO $pdo, string $messageSubject) {
-			// sanitize the description before seaching
-			$messageContent = trim($messageContent);
-			$messageSubject = filter_var($messageSubject, FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-			if(empty($messageSubject) === true)
-				throw(new\PDOException("message subject is invalid"));
-		}
-			// create query template
-			$query = "SELECT messageId, senderId, productId, messageContent, recipientId, messageMailGunId, messageSubject FROM message WHERE messageContent LIKE :messageContent";
-			$statement = $pdo->prepare($query);
-
-			// bind the message subject to the place holder in the template
-			$messageSubject= "%$messageContent%";
-			$parameters = array("messageSubject" => $messageContent);
-			$statement->execute($parameters);
-
-			// build an array of messages
-			$message = new \SPLFixedArray($statement->rowCount());
-			$statement->setFetchMode(\PDO::FETCH_ASSOC);
-			while(($row = $statment->fetch()) !== false) {
-				try {
-					$message = new Messaage($row["messageId"], $row["senderId"], $row["productId"], $row["messageContent"], $row["recipientId"], $row["messageMailGunId"], $row["messageSubject"]);
-					$messages[$message->key()] = $message;
-					$message->next();
-				} catch(\Exception $exception) {
-					// if the row couln't be converted, rethrow it
-					throw(new ]PDOExceptions($exception->getMesage(), 0, $exceptions));
-				}
-			}
-			return($messages);
-		}
-	} /* end Message Class */
+}
