2ab91f84bb1b2b9dbe06b70f34fa0cd24b892a49
Marlan Ball <wyndows2k@earthlink.net>
2016-05-19 19:06:00 -0600

    hopefully ready for burn down

--- public_html/test/PurchaseTest.php
+++ public_html/test/PurchaseTest.php
@@ -143,18 +143,13 @@ class PurchaseTest extends CartridgeCodersTest {
 		$purchase = new Purchase(null, $this->purchaseAccountId->getAccountId(), $this->VALID_PAYPALTRANSACTIONID, $this->VALID_PURCHASECREATEDATE);
 		$purchase->insert($this->getPDO());
 
-		//grab the data from mySQL and enforce the fields match our expectations
-		$results = Purchase::getPurchaseByPurchasePayPalTransactionId($this->getPDO(), $purchase->getPurchasePayPalTransactionId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("purchase"));
-		$this->assertCount(0, $results);
-		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\CartridgeCoders\\Purchase", $results);
-
 		// grab the data from mySQL and enforce the fields match out expectations
-		$pdoPurchase = Purchase::getPurchaseByPurchaseId($this->getPDO(), $purchase->getPurchaseId());
+		$pdoPurchase = Purchase::getPurchaseByPurchasePayPalTransactionId($this->getPDO(), $purchase->getPurchasePayPalTransactionId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("purchase"));
 		$this->assertEquals($pdoPurchase->getPurchaseAccountId(), $this->purchaseAccountId->getAccountId());
 		$this->assertEquals($pdoPurchase->getPurchasePayPalTransactionId(), $this->VALID_PAYPALTRANSACTIONID);
 		$this->assertEquals($pdoPurchase->getPurchaseCreateDate(), $this->VALID_PURCHASECREATEDATE);
 	}
+
 	}
 ?>
