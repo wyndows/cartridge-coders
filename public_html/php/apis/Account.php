ac9567f771091e769c3ef839a027879994554ffe
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-21 12:09:42 -0600

    Account API draft update and save point - https://app.asana.com/0/117435671931140/118836678397130

--- public_html/php/apis/Account.php
+++ public_html/php/apis/Account.php
@@ -1,6 +1,57 @@
 <?php
 
-require_once "autoloader.php";
+require_once "../classes/autoload.php";
+require_once "../lib/xsrf.php";
+require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Edu\Cnm\CartridgeCoders\DataDesign;
+
+/**
+ * api for Account Class
+ *
+ * @author Donald Deleeuw <donald.deleeuw@gmail.com> based on code by Derek Mauldin <derek.e.mauldin@gmail.com>
+ *
+ */
+
+// verify the session, start if not active
+if(session_start() !== PHP_SESSION_ACTIVE){
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try{
+	// grab the mySQL connection
+	$pdo=connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cartridge.ini");
+	
+	// determine which HTTP method was used
+	$method =
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
