1bb9a7ed79b4da24a5e19fceb713d2bf24e5cbac
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-22 23:55:48 -0600

    added the wireframes for the desktop and mobile https://app.asana.com/0/117435667927150/118836678397134

--- public_html/epic/wirframe.php
+++ public_html/epic/wirframe.php
@@ -1,6 +1,14 @@
-
-<p class="title">ROMuLess</p>
-<br>
-<img class="romuless" src="../image/ROMuLessDesktop.svg" alt="desktopWireframe">
-<br>
-<img class="romuless" src="../image/ROMuLessMobile.svg" alt="mobileWireframe">
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="../css/style.css">
+		<title>Cartridge Coders</title>
+	</head>
+	<body><p class="title">ROMuLess</p>
+		<br>
+		<img class="romuless" src="../image/ROMuLessDesktop.svg" alt="desktopWireframe">
+		<br>
+		<img class="romuless" src="../image/ROMuLessMobile.svg" alt="mobileWireframe">
+	</body>
+</html>
