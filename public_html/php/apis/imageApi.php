7de847014a71faef8a24eaf19ec8c56b7469bb57
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-23 00:06:52 -0600

    started the api for image https://app.asana.com/0/117435667927150/118836678397134

---
+++ public_html/php/apis/imageApi.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once "autoloader.php";
+require_once "/lib/xsrf.php";
+require_once("//apache2/cartridge-coders-mysql/encrypted-config.php");
+
+use Edu\Cnm\CartridgeCoders;
+
+/**
+ * api for the image class
+ *
+ * @author Elliot Murrey <emurrey@cnm.edu> parts of this code have been modified from Derik Mauldin <deriek.e.mauldin from @see https://bootcamp-coders.cnm.edu/class-materials/php/writing-restful-apis/
+ **/
+
+//
