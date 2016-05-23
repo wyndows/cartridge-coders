6e988c8e99b7449e8d805d109cd110da21e7d13b
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-22 22:42:18 -0600

    created partials folder and set the index.php for it so that the head, header, side-panel, and footer can be require_once loaded https://app.asana.com/0/117435667927150/118836678397134

--- public_html/index.php
+++ public_html/index.php
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>ROMuLess</title>
-	</head>
-	<body>
-		Welcome to ROMuLess.com
-		A site for selling and buying game cartridges from the consoles of days gone by.
-	</body>
+<!-- head utils contains the entire <head> tag -->
+<?php require_once("php/partials/head-utils.php");?>
+
+<body class="sfooter">
+	<div class="sfooter-content">
+
+		<!-- header partial gets inserted -->
+		<?php require_once("php/partials/header.php");?>
+
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+
+						<!-- side panel get inserted -->
+						<?php require_once("php/partials/side-panel.php");?>
+
+					</div>
+					<div class="col-md-9">
+						main content section - we will use Angular here.
+					</div>
+				</div>
+			</div><!-- ?.container-->
+		</main>
+	</div> <!--/.sfooter-content-->
+	<!--footer get inserted -->
+	<?php require_once("php/partials/footer.php");?>
+
+</body>
 </html>
+
+
