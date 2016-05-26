824a5b8307ff6afbbcf33b003efaab419bfeb993
Marlan Ball <wyndows2k@earthlink.net>
2016-05-26 17:47:29 -0600

    created message api and .htaccess https://app.asana.com/0/117435671931096/118836678397133

---
+++ public_html/php/apis/message/index.php
@@ -0,0 +1,13 @@
+<?php
+use Edu\Cnm\CartridgeCoders;
+
+require_once dirname(__DIR__, 2) . "/classes/autoload.php";
+require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+
+/**
+ * api for the Message class
+ *
+ * @author Marlan Ball <wyndows2k@earthlink.net> based on code by Derek Mauldin
+ **/
