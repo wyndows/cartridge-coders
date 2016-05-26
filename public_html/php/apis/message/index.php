f81ec08313efbdeb24a0f6e6b5869bc9933149c7
Marlan Ball <wyndows2k@earthlink.net>
2016-05-26 17:49:45 -0600

    working on message api  https://app.asana.com/0/117435671931096/118836678397133

--- public_html/php/apis/message/index.php
+++ public_html/php/apis/message/index.php
@@ -11,3 +11,13 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
  *
  * @author Marlan Ball <wyndows2k@earthlink.net> based on code by Derek Mauldin
  **/
+
+//verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
