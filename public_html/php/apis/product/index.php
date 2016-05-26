10e172a2ebfe07a77c49d0650bc9e1b780ebdba7
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-26 08:03:05 -0600

    https://app.asana.com/0/117435667927150/118836678397131 crated product api

--- public_html/php/apis/product/index.php
+++ public_html/php/apis/product/index.php
@@ -7,5 +7,33 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 
 /**
- * 
+ * api for hte product class
+ *
+ * @author Elliot Murrey <emurrey@cnm.edu> parts of this code are modified from a code that was modified from the original created by Derik Mauldin <deriek.e.mauldin>
  **/
+
+// verify the session, start if not active
+if(sessiion_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+// prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null:
+
+try {
+	// grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cartride.ini");
+
+	// determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTT_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+	// make sure the id is valid for methods that require it
+	if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
+	}
+}
