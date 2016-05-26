35eb879ce5a9f1f40a311fbbbf0cba19935807e8
Marlan Ball <wyndows2k@earthlink.net>
2016-05-26 17:57:14 -0600

    added data validation to message api  https://app.asana.com/0/117435671931096/118836678397133

--- public_html/php/apis/message/index.php
+++ public_html/php/apis/message/index.php
@@ -21,3 +21,21 @@ if(session_status() !== PHP_SESSION_ACTIVE) {
 $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
+
+try {
+	//grab the mySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cartridge.ini");
+
+	//determine which HTTP method was used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize input
+	$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+	$senderId = filter_input(INPUT_GET, "senderId", FILTER_VALIDATE_INT);
+	$recipientId = filter_input(INPUT_GET, "recipientId", FILTER_VALIDATE_INT);
+	$productId = filter_input(INPUT_GET, "productId", FILTER_VALIDATE_INT);
+	$content = filter_input(INPUT_GET, "content", FILTER_SANITIZE_STRING);
+	$mailgunId = filter_input(INPUT_GET, "mailgunId", FILTER_SANITIZE_STRING);
+	$subject = filter_input(INPUT_GET, "subject", FILTER_SANITIZE_STRING);
+
+	
