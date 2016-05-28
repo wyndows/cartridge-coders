ace26710f99405efb75db96e3d2bb4c9f9fa44da
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-28 16:33:59 -0600

    Purchase/PayPal API draft update and save point - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/curl.php
+++ public_html/curl.php
@@ -25,55 +25,102 @@
 		require_once(dirname(__DIR__) . "/vendor/autoload.php");
 		$authCode = filter_input(INPUT_GET, "code", FILTER_SANITIZE_STRING);
 
-		//		echo $authCode;
+		// ----- TEMP VERIFY DATA - TO BE REMOVED
+				echo nl2br("authCode: \n");
+				print_r($authCode);
+		echo nl2br("\n");
+		echo nl2br("\n");
 
 
 //------------------------------------------------ cURL ------------------------------------------------
 // ----- @see https://developer.paypal.com/docs/api/#identity & http://incarnate.github.io/curl-to-php/
-		
 
-// ----- cURL - initialize
 
+// ----- cURL - initialize session - get access token from authorization code
 		$ch = curl_init();
 
 		// ----- cURL - set options
-
 		curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.paypal.com/v1/identity/openidconnect/tokenservice");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=AWoiHG8w-yaeYyODSBIzJ-awWkLVPo7G9zWJMomAFeMTVw5wyRG_b2pyYxl7a7wB7ByjVLJ0aQ6FdVDj&client_secret=EDRJgYO1yO0_Y_ligCIUIomHjaGRiXyuWt5lYr7W4pupqxyyC_iK_1N36MYB3jdiKxCp6JAyzxg5a2FE&grant_type=authorization_code&code=".$authCode);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=AWoiHG8w-yaeYyODSBIzJ-awWkLVPo7G9zWJMomAFeMTVw5wyRG_b2pyYxl7a7wB7ByjVLJ0aQ6FdVDj&client_secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx&grant_type=authorization_code&code=".$authCode);
 		curl_setopt($ch, CURLOPT_POST, 1);
 
-		$result = curl_exec($ch);
+		// ----- cURL - get results
+		$accessToken = curl_exec($ch);
 
 		// ----- cURL - error checking
-
 		if (curl_errno($ch)) {
 			echo 'Error:' . curl_error($ch);
 		}
 
 		// ----- cURL - close session
+		curl_close ($ch);
 
+		// ----- TEMP VERIFY DATA - TO BE REMOVED
+		echo nl2br ("accessToken JSON: \n");
+				print_r($accessToken);
+		echo nl2br("\n");
+		echo nl2br("\n");
+
+// ------ break apart return JSON data in $accessToken
+
+// ------ http://stackoverflow.com/questions/4343596/parsing-json-file-with-php
+//		$jsonIterator = new RecursiveIteratorIterator(
+//			new RecursiveArrayIterator(json_decode($accessToken, TRUE)),
+//			RecursiveIteratorIterator::SELF_FIRST);
+//
+//
+//		echo nl2br ("accessToken JSON broken down: \n");
+//		foreach ($jsonIterator as $key => $val) {
+//			if(is_array($val)) {
+//				echo nl2br ("$key:\n");
+//			} else {
+//				echo nl2br("$key => $val\n");
+//			}
+//		}
+
+		var_dump($accessToken);
+
+
+
+
+		$accessTokenExtract = json_decode($accessToken, true);
+
+		$accessTokenExtractType =
+		$accessTokenExtractToken =
+
+
+			// ----- cURL - get user attributes
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, "https://api.sandbox.paypal.com/v1/identity/openidconnect/userinfo");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+
+
+
+		$headers = array();
+		$headers[] = "Authorization:Bearer ".$accessTokenExtract;
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+
+		$userAttributes = curl_exec($ch);
+		if (curl_errno($ch)) {
+			echo 'Error:' . curl_error($ch);
+		}
 		curl_close ($ch);
 
 
-		// ----- cURL - close session
+		// ----- cURL - verify data (REMOVE LATER)
+		echo nl2br ("userAttributes: \n");
+		print_r($userAttributes);
+		echo nl2br("\n");
+		echo nl2br("\n");
 
-//		echo '<pre>';
-		print_r($result);
-
-
-
-
-//<h2>Your Data</h2>;
-//<p><b>Name:</b> echo </p>;
-//<p><b>Email:</b> </b> echo </p>;
-//<p><b>Code:</b> </b> echo </p>;
-//<p><b>Token:</b> </b> echo </p>;
-//
-
-
-//		<hr/>;
-		echo $authCode;
+var_dump($accessTokenExtractToken);
+		var_dump($accessTokenExtractType);
+		var_dump($accessTokenExtract);
+		var_dump($userAttributes);
 
 ?>
 
