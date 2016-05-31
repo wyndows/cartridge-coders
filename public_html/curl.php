9946b9f9346a37188bd7a48292ef122ad5bf45e0
Don Deleeuw <donald.deleeuw@gmail.com>
2016-05-31 16:52:19 -0600

    Purchase/PayPal API draft update and save point - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/curl.php
+++ public_html/curl.php
@@ -103,17 +103,17 @@
 		var_dump($userAttributesEmail);
 		var_dump($userAttributesFirstName);
 
-		//--------------------------------------------- mySQL lookup/new ---------------------------------------------
+		//--------------------------------------------- mySQL -------------------------------------------------------
 
-		require_once dirname(__DIR__, 2) . "/classes/autoload.php";
-		require_once dirname(__DIR__, 2) . "/lib/xsrf.php";
+		require_once dirname(__DIR__) . "/public_html/php/classes/autoload.php";
+		require_once dirname(__DIR__) . "/public_html/php/lib/xsrf.php";
 		require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 		use Edu\Cnm\CartridgeCoders;
 
 
 		// verify the session, start if not active
-		if(session_start() !== PHP_SESSION_ACTIVE) {
+		if(session_status() !== PHP_SESSION_ACTIVE) {
 			session_start();
 		}
 
@@ -122,119 +122,26 @@
 		$reply->status = 200;
 		$reply->data = null;
 
-//		try {
-//			// grab the mySQL connection
-//			$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cartridge.ini");
-//
-//			// determine which HTTP method was used
-//			$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
-//
-//			//sanitize input
-//			$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
-//
-//			// make sure the id is valid for methods that require it
-//			if(($method === "DELETE" || $method === "PUT") && (empty($id) === true || $id < 0)) {
-//				throw(new InvalidArgumentException("id can not be empty or negative", 405));
-//			}
-//
-//			// handle GET request - if id is present, that account is returned, otherwise all accounts are returned
-//			if($method === "GET") {
-//				// set XSRF cookie
-//				setXsrfCookie();
-//
-//				// get a specific account or all accounts and update reply
-//				if(empty($id) === false) {
-//					$account = CartridgeCoders\Account::getAccountByAccountId($pdo, $id);
-//					if($account !== null) {
-//						$reply->data = $account;
-//					}
-//				} else {
-//					$accounts = CartridgeCoders\Account::getAllAccounts($pdo);
-//					if($accounts !== null) {
-//						$reply->data = $accounts;
-//					}
-//				}
-//			} else if($method === "PUT" || $method === "POST") {
 
-//				verifyXsrf();
-//				$requestContent = file_get_contents("php://input");
-//				$requestObject = json_decode($requestContent);
-//
-//				// make sure account content is available
-//				if(empty($requestObject->accountName) === true) {
-//					throw(new \InvalidArgumentException ("No content for account - get it together already.", 405));
-//				}
-//
-//
-//				// perform actual put or post
-//				if($method === "PUT") {
-//
-//					// retrieve the account to update
-//					$account = CartridgeCoders\Account::getAccountByAccountId($pdo, $id);
-//					if($account === null) {
-//						throw(new RuntimeException("Account does not exist", 404));
-//					}
-//
-//					// put the new Account content into the account and update
-//					$account->setAccountContent($requestObject->accountImageId, $requestObject->accountActive, $requestObject->accountAdmin, $requestObject->accountName, $requestObject->accountPpEmail, $requestObject->accountUserName);
-//					$account->update($pdo);
-//
-//					// update reply
-//					$reply->message = "Account updated OK";
-//
-//				} else if($method === "POST") {
-//
-//					// make sure accountIf is available
-//					if(empty($requestObject->accountImageId) === true) {
-//						throw(new \InvalidArgumentException ("No Account Id", 405));
-//					}
 
-					// create new account and insert into database
-//					$account = new CartridgeCoders\Account(null, $requestObject->accountImageId, $requestObject->accountActive, $requestObject->accountAdmin, $requestObject->accountName, $requestObject->accountPpEmail, $requestObject->accountUserName);
-//					$account->insert($pdo);
-//
-//					// update reply
-//					$reply->message = "Account created OK";
-//				}
-//
-//
-//			} else if($method === "DELETE") {
-//				verifyXsrf();
-//
-//				// retrieve the Account to be deleted
-//				$account = CartridgeCoders\Account::getAccountByAccountId($pdo, $id);
-//				if($account === null) {
-//					throw(new RuntimeException("Account does not exist", 404));
-//				}
-
-				// delete account
-//				$account->delete($pdo);
-
-				// update reply
-//				$reply->message = "Account deleted ok";
-//			} else {
-//				throw (new InvalidArgumentException("Invalid HTTP method request"));
-//			}
-
-			// update reply with exception information
-//		} catch(Exception $exception) {
-//			$reply->status = $exception->getCode();
-//			$reply->message = $exception->getMessage();
-//			$reply->trace = $exception->getTraceAsString();
-//		} catch(TypeError $typeError) {
-//			$reply->status = $typeError->getCode();
-//			$reply->message = $typeError->getMessage();
-//		}
-
-//		header("Content-type: application/json");
-//		if($reply->data === null) {
-//			unset($reply->data);
-//		}
+// ----------------------------------- mySQL lookup/new ----------------------------------
 
 
 
-		// encode and return reply to front end caller
-		echo json_encode($reply);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 		?>
