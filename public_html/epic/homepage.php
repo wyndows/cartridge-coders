7f8fdf54d838a269ff06d3101da3075ab76addaf
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-01 23:37:33 -0600

    https://app.asana.com/0/117435667927150/118836678397131this is so much fun

--- public_html/epic/homepage.php
+++ public_html/epic/homepage.php
@@ -31,10 +31,10 @@
 		<div class="sfooter">
 			
 			<!-- header -->
-			<header class="ROMuLesscool">
+			<header class="p-y-4">
 				<div class="container">
 					<!-- brand and toggle stuff-->
-					<nav class="navbar navbar-default">
+					<nav class="navbar navbar-inverse">
 						<div class="container-fluid">
 							<!-- Brand and toggle get grouped for better mobile display -->
 							<div class="navbar-header">
@@ -49,11 +49,6 @@
 
 							<!-- Collect the nav links, forms, and other content for toggling -->
 							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-								<ul class="nav navbar-nav">
-									<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-									<li><a href="#">Link</a></li>
-									<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-								</ul>
 								<form class="navbar-form navbar-left" role="search">
 									<div class="form-group">
 										<input type="text" class="form-control" placeholder="">
