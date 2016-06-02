7e0c13e3119dde616d16cfce46eda75da5b44396
Don Deleeuw <donald.deleeuw@gmail.com>
2016-06-02 15:18:22 -0600

    Purchase/PayPal API draft update and save point - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/return.php
+++ public_html/return.php
@@ -186,10 +186,9 @@
 		<!-- PayPal Checkout Button begin -------------------------------------------------------------  -->
 
 
-
-
-
-
+		<form action='expresscheckout.php' METHOD='POST'>
+			<input type='image' name='submit' src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif' border='0' align='top' alt='Check out with PayPal'/>
+		</form>
 
 
 		<!-- PayPal Checkout Button begin -------------------------------------------------------------  -->
