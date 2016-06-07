f8492d9eeaf899a0f8a7adfe279f1a799c16ea53
Don Deleeuw <donald.deleeuw@gmail.com>
2016-06-07 09:58:13 -0600

    Purchase/PayPal API draft update and save point - https://app.asana.com/0/117435671931140/118836678397128

--- public_html/angular/views/home.php
+++ public_html/angular/views/home.php
@@ -6,7 +6,20 @@
 <div class="well homecss">
 <div class="row">
 	<div class="col-md-2"><img src="../public_html/image/cartridge/avatar01.jpg" alt="2600"  class="img-responsive img-rounded"></div>
-	<div class="col-md-1"></div>
+	<div class="col-md-1">
+
+<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=MDCF4WD82ZLPE&lc=US&item_name=TEST%20ITEM&item_number=TESTid&amount=999%2e99&currency_code=USD&button_subtype=services&no_note=0&cn=Add%20special%20instructions%20to%20the%20seller%3a&no_shipping=2&rm=1&return=https%3a%2f%2fbootcamp%2dcoders%2ecnm%2eedu%2f%7eddeleeuw%2fcartridge%2dcoders%2fpublic_html%2findex%2ephp&cancel_return=https%3a%2f%2fbootcamp%2dcoders%2ecnm%2eedu%2f%7eddeleeuw%2fcartridge%2dcoders%2fpublic_html%2findex%2ephp&bn=PP%2dBuyNowBF%3abtn_buynowCC_LG%2egif%3aNonHosted">
+
+	<img src="../../image/paypal-buynow-button.png">
+
+
+</a>
+
+
+
+
+
+	</div>
 	<div class="col-md-9">Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description Game Description </div>
 </div>
 	</div>
